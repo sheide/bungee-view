60b4b7dd8ac6d4202f0eb09d7263a59eb93a8af8
markderthick <markderthick@c699882f-f239-0410-9b47-a903b765d604>
2009-03-26 23:30:54 +0000

    Switching to Thinkpad

--- trunk/javaExtensions/edu/cmu/cs/bungee/javaExtensions/PermutationIterator.java
+++ trunk/javaExtensions/edu/cmu/cs/bungee/javaExtensions/PermutationIterator.java
@@ -16,7 +16,7 @@ public class PermutationIterator implements Iterator {
 	private int nRemainingPerms;
 	private final List perm;
 
-	public PermutationIterator(List collection) {
+	public PermutationIterator(Collection collection) {
 		objects = collection.toArray();
 		int nObjects = objects.length;
 		integerPermGenerator = new IntegerPermutationGenerator(nObjects);
